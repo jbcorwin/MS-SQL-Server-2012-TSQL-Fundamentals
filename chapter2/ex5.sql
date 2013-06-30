7df31ac527058381b5257efa3cc2e48d3c2b883e
Jesse Corwin <aiskunkworks@gmail.com>
2013-06-30 08:53:42 -0500

    Solution for example 5
    tl;dr

---
+++ chapter2/ex5.sql
@@ -0,0 +1,13 @@
+-- Write a query against Sales.Orders that returns the three shipped-to with the
+--- highest average freight in 2007
+-- Tables Involved: Sales.Orders
+-- Desired columns: shipcountry, avgfreight
+
+USE TSQL2012;
+GO
+
+SELECT TOP 3 shipcountry, AVG(freight) as 'avgfreight'
+FROM Sales.Orders
+WHERE YEAR(orderdate) = 2007
+GROUP BY shipcountry
+ORDER BY avgfreight DESC;
